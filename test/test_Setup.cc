1aa3176dc2d657447b1c503673a25cc5d8c303a2
David Millman <dave@cs.unc.edu>
2018-08-10 19:55:10 -0500

    Refactor test setup

---
+++ test/test_Setup.cc
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+#include "setup.h"
+
+TEST(FooTest, Example) {
+    EXPECT_EQ(15.0, foo(7, 8));
+}
+
+TEST(FooTest, Example2) {
+    EXPECT_EQ(15.0, foo(7, 8));
+}
+
+
+
