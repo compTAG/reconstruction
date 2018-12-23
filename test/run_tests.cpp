f73c63dc4ed1097b96354f3a207bb091a1696931
David Millman <dave@cs.unc.edu>
2018-12-23 15:53:15 -0500

    Remove code not for oracle

--- test/run_tests.cpp
+++ test/run_tests.cpp
@@ -1,14 +1,13 @@
 #include "gtest/gtest.h"
 
 #include "test_height_function.cc"
-#include "test_oracle.cc"
-#include "test_filtration_line.cc"
 #include "test_filtration_factory.cc"
 #include "test_diagram.cc"
 #include "test_diagram_factory.cc"
+#include "test_oracle.cc"
+
 #include "test_euler_characteristic_curve.cc"
 #include "test_euler_characteristic_curve_factory.cc"
-#include "test_vertex_reconstructor.cc"
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
