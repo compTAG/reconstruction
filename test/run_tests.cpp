c814c589403b467d0d4b240950785bd8092641d3
David Millman <dave@cs.unc.edu>
2018-12-23 16:02:06 -0500

    Add filtration line

--- test/run_tests.cpp
+++ test/run_tests.cpp
@@ -6,6 +6,8 @@
 #include "test_diagram_factory.cc"
 #include "test_oracle.cc"
 
+#include "test_filtration_line.cc"
+
 #include "test_euler_characteristic_curve.cc"
 #include "test_euler_characteristic_curve_factory.cc"
 
