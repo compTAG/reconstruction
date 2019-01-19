8d0ad6f535381c4b648937e68a7e70d90dd44de3
David Millman <dave@cs.unc.edu>
2019-01-18 21:37:25 -0700

    Add first version of reconstructor

--- test/run_tests.cpp
+++ test/run_tests.cpp
@@ -15,6 +15,7 @@
 
 #include "test_vertex_reconstructor.cc"
 #include "test_edge_reconstructor.cc"
+#include "test_reconstructor.cc"
 
 #include "test_euler_characteristic_curve.cc"
 #include "test_euler_characteristic_curve_factory.cc"
---
+++ test/test_reconstructor.cc
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+
+#include "ctag/oracle.h"
+#include "ctag/reconstructor.h"
+
+class ReconstructorTest : public ::testing::Test {
+public:
+    typedef ctag::Oracle Oracle;
+
+    typedef Oracle::Point Point;
+    typedef Oracle::Simplex Simplex;
+
+    typedef ctag::Reconstructor<Oracle> Reconstructor;
+};
+
+
+TEST_F(ReconstructorTest, reconstruct_basic_example) {
+    Point p1({0,0});
+    Point p2({2,2});
+    Point p3({1,3});
+    Point p4({4,1});
+
+    Simplex t_e1({p1, p2});
+    Simplex t_e2({p2, p3});
+    Simplex t_e3({p3, p1});
+    Simplex e1({p2, p4});
+
+    Oracle oracle({ t_e1, t_e2, t_e3, e1 });
+
+    std::vector<Simplex> result;
+    Reconstructor reconstructor;
+    reconstructor.reconstruct(std::back_inserter(result), oracle);
+
+    for (auto si : result) {
+        std::cout << si << std::endl;
+    }
+};
