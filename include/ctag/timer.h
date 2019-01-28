fef840e9c8d5ddd6b3f60defd121cfda1ea0faf9
David Millman <dave@cs.unc.edu>
2019-01-27 17:11:56 -0700

    Add timer wrapper

---
+++ include/ctag/timer.h
@@ -0,0 +1,20 @@
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+#include "plf/nanotimer.h"
+
+namespace ctag {
+class Timer {
+protected:
+
+	plf::nanotimer _timer;
+    double _delta;
+
+public:
+    Timer() : _delta(0) {}
+    void start() { _timer.start(); }
+    void stop() { _delta += _timer.get_elapsed_ns(); }
+    double total() const { return _delta; }
+};
+};
+#endif
